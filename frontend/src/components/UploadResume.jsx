627b3e1180a5892ae09d7f448110344b602641b7
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-07-15 01:11:06 +0530

    Minor bug fix

--- frontend/src/components/UploadResume.jsx
+++ frontend/src/components/UploadResume.jsx
@@ -14,7 +14,7 @@ function UploadResume() {
 
     setLoading(true);
     try {
-      const res = await axios.post("https://simple-resume-analyzer-backand.onrender.com", formData);
+      const res = await axios.post("https://simple-resume-analyzer-backand.onrender.com/upload", formData);
       setData(res.data);
     } catch (error) {
       console.error(error);
